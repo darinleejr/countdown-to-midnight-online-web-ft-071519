937c6e1c61c5a3f5d3ae93b7a2394ee6b9f6a6c1
Darin Webster <darinleenavy@gmail.com>
2019-07-29 21:38:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,45 @@
-#write your code here
+# #write your code here
 
-def countdown
+
+# def countdown(seconds)
+#   while seconds > 0 
+#   puts "#{seconds} SECOND(S)!"
+#   seconds -= 1
+#   end
+#   return "HAPPY NEW YEAR!"
+# end
+
+  
+# def countdown_with_sleep(seconds)
+#   while seconds > 0 
+#   sleep 1
+#   puts "#{seconds} SECOND(S)!"
+#   seconds -= 1
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def countdown(seconds)
+  while seconds > 0 
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+  elsif seconds == 12 
+  puts "HAPPY NEW YEAR!"
+  end
 end
